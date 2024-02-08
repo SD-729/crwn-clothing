833a478ad48c2fd7971fcc589e723dfcfca2041b
Dmnsfo <sofiad.media@gmail.com>
2024-02-08 13:44:13 +0100

    created sign up component, the form input component, button component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -16,7 +16,7 @@ const Navigation = () => {
             SHOP
           </Link>
           <Link className="nav-link" to="/sign-in">
-            SIGNIN
+            SIGN IN
           </Link>
         </div>
       </div>
